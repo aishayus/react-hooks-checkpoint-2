82429952b37a6d26dbb466d4f8e5787bb5f462e2
aishayus <aishayussuff2005@gmail.com>
2024-08-02 12:04:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieDetail from './components/MovieDetail';
 
 function App() {
   return (
-    <>
-      <MovieList />
-
-      
-    </>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/movie/:title" element={<MovieDetail />} />
+      </Routes>
   );
 }
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,89 +1,35 @@
-    import React, { useState } from 'react';
-    import MovieCard from './MovieCard';
-    import Movies from './moviesList';
+import React, { useState } from 'react';
+import MovieCard from './MovieCard';
+import Movies from './moviesList';
+import Filter from './Filter';
+// import AddMovie from './AddMovie';
+
 
     const MovieList = () => {
     const [search, setSearch] = useState('');
-    const [movies, setMovies] = useState(Movies);
-    const [newMovie, setNewMovie] = useState({
-        title: '',
-        description: '',
-        posterURL: '',
-        rating: '',
-    });
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNewMovie({ ...newMovie, [name]: value });
-    };
-
-    const handleAddMovie = (e) => {
-        e.preventDefault();
-        if (newMovie.title && newMovie.description && newMovie.posterURL && newMovie.rating) {
-        setMovies([...movies, { ...newMovie, id: movies.length + 1 }]);
-        setNewMovie({
-            title: '',
-            description: '',
-            posterURL: '',
-            rating: '',
-        });
-        }
-    };
+    // const [movies, setMovies] = useState(Movies);
+    // const handleAddMovie = (newMovie) => {
+    //     setMovies([...movies, { ...newMovie, id: movies.length + 1 }]);
+    // };
 
     return (
         <div>
-        <form className='form'>
-            <input
-            placeholder='eg pretty woman...'
-            onChange={(e) => setSearch(e.target.value)}
-            />
-        </form>
-        
+            
+        <Filter setSearch={setSearch} />
+
         <div className='movie'>
-            {movies
+            {Movies
             .filter((movie) => {
                 return search.toLowerCase() === ''
                 ? movie
                 : movie.title.toLowerCase().includes(search);
             })
             .map((movie) => {
-                return <MovieCard movie={movie} />;
+                return <MovieCard key={movie.id} movie={movie} />;
             })}
         </div>
-        <form className='newMovie' onSubmit={handleAddMovie}>
-            <input
-            type='text'
-            name='title'
-            placeholder='Title'
-            value={newMovie.title}
-            onChange={handleInputChange}
-            />
-            <input
-            type='text'
-            name='description'
-            placeholder='Description'
-            value={newMovie.description}
-            onChange={handleInputChange}
-            />
-            <input
-            type='text'
-            name='posterURL'
-            placeholder='Poster URL'
-            value={newMovie.posterURL}
-            onChange={handleInputChange}
-            />
-            <input
-            type='number'
-            name='rating'
-            placeholder='Rating'
-            value={newMovie.rating}
-            onChange={handleInputChange}
-            />
-            <button type='submit'>Add Movie</button>
-        </form>
+        {/* <AddMovie handleAddMovie={handleAddMovie} /> */}
         </div>
-
-        
     );
     };
 
